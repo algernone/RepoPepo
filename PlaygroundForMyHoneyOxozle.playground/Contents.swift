36fe77c937be06258a833eadaa67e9d78e108b23
Alexander Polezhaev <alexander@Alexanders-MacBook-Air.local>
2017-05-29 22:32:16 +0300

    working reverse

--- PlaygroundForMyHoneyOxozle.playground/Contents.swift
+++ PlaygroundForMyHoneyOxozle.playground/Contents.swift
@@ -4,3 +4,30 @@ import UIKit
 
 var str = "Hello, playground"
 print ("hey dude")
+var superArray: [Int] = [0,1,2,3,4,5,6]
+var size = superArray.count
+var temp = 0
+var step = 0
+
+
+if (size % 2 == 0) {
+    step = size / 2
+}
+else {
+    step = size / 2 - 1
+}
+print ("here step")
+print (step)
+size = size - 1
+for i in 0...step {
+    temp = superArray[i]
+    superArray[i] = superArray[size - i]
+    superArray[size - i] = temp
+}
+print (superArray)
+
+
+
+
+
+
